a211b840445dfe090d415a06cae899c93dea6f98
sajjathossain <painlesssajjad@gmail.com>
2020-10-24 19:30:23 +0600

    updated

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, redirect, request
+import youtube_dl
 
 app = Flask(__name__)
+
+ydl_opts = {}
 
 @app.route("/")
 @app.route("/home")
@@ -17,6 +20,19 @@ def terms():
 @app.route("/privacy-policy.html")
 def privacy():
     return render_template("privacy-policy.html")
+
+
+@app.route("/download.html", methods=["GET","POST"])
+def download():
+    uLink = request.form['url']
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        url = ydl.extract_info(uLink, download=False)
+        downloadLink = (url["formats"][-1]["url"])
+        
+        # downloadLink = uLink.strip()
+        # ydl.download([downloadLink])
+        
+    return redirect(downloadLink+"&dl=1")
 
 
 if __name__ == "__main__":
